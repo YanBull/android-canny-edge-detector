478c9a1d7ecbf0a32bb6109bc8eb8de579678790
Yan Bulatov <Yan.Bulatov@deutschebahn.com>
2025-01-06 20:24:16 +0100

    init

---
+++ app/src/main/java/com/example/CannyFilter2/ui/theme/CannyAnalyzer.kt
@@ -0,0 +1,66 @@
+package com.example.CannyFilter2.ui.theme
+
+import android.graphics.*
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import org.opencv.android.Utils
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.imgproc.Imgproc
+import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+
+typealias EdgesListener = (edges: Int) -> Unit
+
+
+class CannyAnalyzer(private val listener: EdgesListener) : ImageAnalysis.Analyzer {
+
+    private fun ByteBuffer.toByteArray(): ByteArray {
+        rewind()    // Rewind the buffer to zero
+        val data = ByteArray(remaining())
+        get(data)   // Copy the buffer into a byte array
+        return data // Return the byte array
+    }
+
+    override fun analyze(image: ImageProxy) {
+        val yBuffer = image.planes[0].buffer
+        val uBuffer = image.planes[1].buffer
+        val vBuffer = image.planes[2].buffer
+
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + uSize + vSize)
+
+        yBuffer.get(nv21, 0, ySize)
+        vBuffer.get(nv21, ySize, vSize)
+        uBuffer.get(nv21, ySize + vSize, uSize)
+
+        val yuvImage = YuvImage(nv21, ImageFormat.NV21, image.width, image.height, null)
+        val out = ByteArrayOutputStream()
+        yuvImage.compressToJpeg(Rect(0, 0, image.width, image.height), 100, out)
+        val imageBytes = out.toByteArray()
+        val bmp = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+
+        // Convert Bitmap to Mat
+        val mat = Mat(bmp.height, bmp.width, CvType.CV_8UC3)
+        Utils.bitmapToMat(bmp, mat)
+
+        // Apply Canny Edge Detection
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2GRAY)
+        Imgproc.Canny(mat, mat, 100.0, 200.0)
+
+        val edgeCount = Core.countNonZero(mat)
+        listener(edgeCount)
+
+        // Optional: Convert Mat to Bitmap to display
+//        val cannyBitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.ARGB_8888)
+//        Utils.matToBitmap(mat, cannyBitmap)
+
+        // Close the image proxy to prevent memory leaks
+        image.close()
+    }
+
+}
---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
+
+buildscript {
+    val kotlin_version by extra("1.8.0")
+
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
