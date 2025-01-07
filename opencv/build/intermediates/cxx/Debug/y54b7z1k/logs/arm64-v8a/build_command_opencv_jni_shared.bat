9bc6fb29aa3b1a955583a02e96d2a5a9659ba10b
Yan Bulatov <Yan.Bulatov@deutschebahn.com>
2025-01-08 01:04:04 +0500

    delete local opencv

--- opencv/build/intermediates/cxx/Debug/y54b7z1k/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\YanBulatov\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\YanBulatov\\AndroidStudioProjects\\CannyFilter2\\opencv\\.cxx\\Debug\\y54b7z1k\\arm64-v8a" ^
-  opencv_jni_shared
